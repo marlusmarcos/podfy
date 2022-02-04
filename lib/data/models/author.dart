dd8760b616f181e6314be63358bd50b68433b2ca
marlusmarcos <marlusufrn@gmail.com>
2022-02-04 14:54:17 -0300

    author e service author

--- lib/data/models/author.dart
+++ lib/data/models/author.dart
@@ -4,4 +4,9 @@ class Author {
   int id;
 
   Author(this.name, this.description, this.id);
+  
+
+  static Author fromJson(Map json) {
+    return Author (json['name'] as String, json['description'] as String, json['id'] as int);
+  }
 }
--- lib/data/services/autor_service.dart
+++ lib/data/services/autor_service.dart
@@ -1,3 +1,23 @@
+import 'package:podfy/data/models/author.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class AutorService {
   // TODO: Listar todos os autores
+  final String urlBase;
+  Author? podcastAtual;
+
+  AutorService(this.urlBase);
+
+  Future<List<Author>> listarAutores() async {
+    try {
+      var url = Uri.parse(urlBase);
+      var res = await http.get(url);
+      return (jsonDecode(res.body) as List)
+          .map((e) => Author.fromJson(e))
+          .toList();
+    } catch (e) {
+      throw Exception('e');
+    }
+  }
 }
